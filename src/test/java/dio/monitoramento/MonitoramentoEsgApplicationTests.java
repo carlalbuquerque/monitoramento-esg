e1206f0aff2aeff1bfad466ddb378fdb0ac61d0a
Carla Albuquerque <ccja@cesar.school>
2025-03-12 11:43:10 -0300

    Initial commit

---
+++ src/test/java/dio/monitoramento/MonitoramentoEsgApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.monitoramento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoramentoEsgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
